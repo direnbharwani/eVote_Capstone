3a4ea7e8b3a803d281046e12c0ed2dd573649955
Diren <diren_d_bharwani_from.tp@tech.gov.sg>
2024-03-28 18:07:23 +0800

    Style changes to components

--- app/client/src/components/CandidateList.svelte
+++ app/client/src/components/CandidateList.svelte
@@ -1,117 +1,112 @@
 <!-- src/components/Candidates.svelte -->
 
 <script>
-    export let candidates = [];
-    export let selectedCandidate;
+  export let candidates = [];
+  export let selectedCandidate;
 
-    export let ballotCast = false;
+  export let ballotCast = false;
 
-    // Verify that candidates have the expected structure
-    if (
-        candidates.some(
-            (obj) =>
-                !obj.hasOwnProperty("CandidateID") ||
-                !obj.hasOwnProperty("Name") ||
-                typeof obj.Voted !== "boolean",
-        )
-    ) {
-        console.error("Invalid candidate object structure!");
+  // Verify that candidates have the expected structure
+  if (
+    candidates.some(
+      (obj) =>
+        !obj.hasOwnProperty("CandidateID") ||
+        !obj.hasOwnProperty("Name") ||
+        typeof obj.Voted !== "boolean",
+    )
+  ) {
+    console.error("Invalid candidate object structure!");
+  }
+
+  // Find the candidate with Voted=true and set it as the selected option
+  candidates.forEach((candidate) => {
+    if (candidate.Voted) {
+      ballotCast = true;
+      selectedCandidate = candidate.CandidateID;
     }
-
-    // Find the candidate with Voted=true and set it as the selected option
-    candidates.forEach((candidate) => {
-        if (candidate.Voted) {
-            ballotCast = true;
-            selectedCandidate = candidate.CandidateID;
-        }
-    });
+  });
 </script>
 
 <div id="radio-buttons">
-    {#each candidates as candidate, index}
-        <div class="radio-container">
-            <input
-                type="radio"
-                id={`option${index}`}
-                name="options"
-                value={candidate.CandidateID}
-                bind:group={selectedCandidate}
-                class="styled-radio"
-                disabled={ballotCast && selectedCandidate}
-            />
-            <label for={`option${index}`} class="radio-label"
-                >{candidate.Name}</label
-            >
-        </div>
-    {/each}
+  {#each candidates as candidate, index}
+    <div class="radio-container">
+      <input
+        type="radio"
+        id={`option${index}`}
+        name="options"
+        value={candidate.CandidateID}
+        bind:group={selectedCandidate}
+        class="styled-radio"
+        disabled={ballotCast && selectedCandidate}
+      />
+      <label for={`option${index}`} class="radio-label">{candidate.Name}</label>
+    </div>
+  {/each}
 </div>
 
 <style>
-    #radio-buttons {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        margin-bottom: 5%;
-    }
+  #radio-buttons {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 5%;
+  }
 
-    .radio-container {
-        display: flex;
-        align-items: center;
-        margin-bottom: 3%; /* Space between radio containers */
-    }
+  .radio-container {
+    display: flex;
+    align-items: center;
+    margin-bottom: 3%; /* Space between radio containers */
+  }
 
-    .styled-radio {
-        display: none; /* Hide the default radio buttons */
-        cursor: pointer;
-    }
+  .styled-radio {
+    display: none; /* Hide the default radio buttons */
+    cursor: pointer;
+  }
 
-    .radio-label {
-        cursor: pointer;
-        margin-right: 10px; /* Space between the label and the radio button */
-        position: relative;
-        font-size: 1.5em;
-        text-transform: capitalize;
-    }
+  .radio-label {
+    cursor: pointer;
+    margin-right: 10px; /* Space between the label and the radio button */
+    position: relative;
+    font-family: Tauri, sans-serif;
+    font-size: 1.5em;
+    text-transform: capitalize;
+  }
 
-    .radio-label::before {
-        content: "";
-        display: inline-block;
-        width: 50px; /* Adjust width of the tick box */
-        height: 50px; /* Adjust height of the tick box */
-        border: 1px solid #000;
-        margin-right: 50px; /* Space between the label and the tick box */
-        vertical-align: middle; /* Align vertically with the text */
-        position: absolute;
-        left: -20%;
-        top: 50%;
-        transform: translateY(-50%);
-        box-sizing: border-box; /* Include border in the size */
-        padding: 2px; /* Space inside the tick box */
-    }
+  .radio-label::before {
+    content: "";
+    display: inline-block;
+    width: 50px; /* Adjust width of the tick box */
+    height: 50px; /* Adjust height of the tick box */
+    border: 1px solid #000;
+    margin-right: 50px; /* Space between the label and the tick box */
+    vertical-align: middle; /* Align vertically with the text */
+    position: absolute;
+    left: -20%;
+    top: 50%;
+    transform: translateY(-50%);
+    box-sizing: border-box; /* Include border in the size */
+    padding: 2px;
+  }
 
-    input[type="radio"] + .radio-label::after {
-        content: "✔";
-        font-family: "lucida grande";
-        font-size: 1.5em; /* Adjust size of the tick mark */
-        line-height: 20px; /* Adjust line-height to vertically center the tick mark */
-        text-align: center;
-        display: none; /* Initially hidden */
-        position: absolute;
-        left: -15%; /* Position inside the tick box */
-        top: 50%; /* Vertically center inside the tick box */
-        transform: translateY(-50%);
-    }
+  input[type="radio"] + .radio-label::after {
+    content: "✔";
+    font-family: "lucida grande";
+    font-size: 1.5em; /* Adjust size of the tick mark */
+    line-height: 20px; /* Adjust line-height to vertically center the tick mark */
+    text-align: center;
+    display: none; /* Initially hidden */
+    position: absolute;
+    left: -15%; /* Position inside the tick box */
+    top: 50%; /* Vertically center inside the tick box */
+    transform: translateY(-50%);
+  }
 
-    input[type="radio"]:checked + .radio-label::after {
-        display: inline-block; /* Show the tick mark when checked */
-    }
+  input[type="radio"]:checked + .radio-label::after {
+    display: inline-block; /* Show the tick mark when checked */
+  }
 
-    /* Adjust the padding for the label to account for the tick box */
-    .radio-label {
-        padding-left: 30px; /* Space for the radio button and the tick box */
-    }
-
-    input[type="radio"] + .radio-label {
-        padding-left: 30px; /* Space for the radio button and the tick box */
-    }
+  /* Adjust the padding for the label to account for the tick box */
+  input[type="radio"] + .radio-label {
+    padding-left: 30px; /* Space for the radio button and the tick box */
+  }
 </style>
--- app/client/src/pages/Ballot.svelte
+++ app/client/src/pages/Ballot.svelte
@@ -1,112 +1,113 @@
 <!-- src/components/Button.svelte -->
 
 <script>
-    import { onMount } from "svelte";
-    import axios from 'axios';
+  import { onMount } from "svelte";
+  import axios from "axios";
 
-    import Button from "../components/Button.svelte";
-    import CandidateList from "../components/CandidateList.svelte";
+  import Button from "../components/Button.svelte";
+  import CandidateList from "../components/CandidateList.svelte";
 
-    let userID = "";
-    let ballotID = "";
+  let userID = "";
+  let ballotID = "";
 
-    let candidates = [];
-    let ballotCast = false;
+  let candidates = [];
+  let ballotCast = false;
 
-    onMount(async () => {
-        userID = sessionStorage.getItem("userID");
-        ballotID = sessionStorage.getItem("ballotID");
+  onMount(() => {
+    userID = sessionStorage.getItem("userID");
+    ballotID = sessionStorage.getItem("ballotID");
 
-        
-        await fetchBallot();
-    });
+    fetchBallot();
+  });
 
-    async function fetchBallot() {
-        try {
-            const response = await axios.post('https://dt1nck5gqd.execute-api.ap-southeast-1.amazonaws.com/dev/read-vote', {
-                VoterID: userID,
-	            BallotID: ballotID
-            });
+  async function fetchBallot() {
+    try {
+      const response = await axios.post(
+        "https://dt1nck5gqd.execute-api.ap-southeast-1.amazonaws.com/dev/read-vote",
+        {
+          VoterID: userID,
+          BallotID: ballotID,
+        },
+      );
 
-            candidates = response.data.Candidates;
-
-        } catch (error) {
-            console.error('No ballot found: ', error);
-        }
+      candidates = response.data.Candidates;
+    } catch (error) {
+      console.error("No ballot found: ", error);
     }
-
+  }
 </script>
 
 <main>
-    <body>
-        <div id="ballot-paper">
-            <!-- Voter and Ballot IDs at the top of the ballot paper -->
-            <div id="ids">
-                <div class="id-item" id="voterID">
-                    <b>VoterID:</b> <span>{userID}</span>
-                </div>
-                <div class="id-item" id="ballotID">
-                    <b>BallotID:</b> <span>{ballotID}</span>
-                </div>
-            </div>
-
-            {#if candidates.length > 0}
-                <CandidateList {candidates} bind:ballotCast />
-            {:else}
-                <h4 align="center">No Candidates Found!</h4>
-            {/if}
+  <body>
+    <div id="ballot-paper">
+      <!-- Voter and Ballot IDs at the top of the ballot paper -->
+      <div id="ids">
+        <div class="id-item" id="voterID">
+          <b>VoterID:</b> <span>{userID}</span>
         </div>
+        <div class="id-item" id="ballotID">
+          <b>BallotID:</b> <span>{ballotID}</span>
+        </div>
+      </div>
 
-        {#if !ballotCast && candidates.length > 0}
-            <Button label="Submit Vote" />
-        {/if}
-    </body>
+      {#if candidates.length > 0}
+        <CandidateList {candidates} bind:ballotCast />
+      {:else}
+        <h4 align="center">No Candidates Found!</h4>
+      {/if}
+    </div>
+
+    {#if !ballotCast && candidates.length > 0}
+      <Button label="Submit Vote" />
+    {/if}
+  </body>
 </main>
 
 <style>
-    body {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
-        height: 100vh;
-        margin: auto;
-        background-color: #f4f4f4;
-    }
+  body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 100vh;
+    margin: auto;
+    background-color: #f4f4f4;
+  }
 
-    #ballot-paper {
-        width: 75%;
-        font-family: Consolas;
-        border: 2px solid;
-    }
+  #ballot-paper {
+    width: 75%;
+    border: 2px solid;
+  }
 
-    #ids {
-        display: grid;
-        grid-template-columns: auto 1fr; /* Label takes auto width, value takes the rest */
-        grid-gap: 0; /* Space between rows */
-        margin-bottom: 5%;
-    }
+  #ids {
+    display: grid;
+    grid-template-columns: auto 1fr; /* Label takes auto width, value takes the rest */
+    grid-gap: 0; /* Space between rows */
+    margin-bottom: 5%;
+    font-family: JetBrains Mono;
+  }
 
-    .id-item {
-        display: flex;
-        align-items: center;
-    }
+  .id-item {
+    display: flex;
+    align-items: center;
+  }
 
-    .id-item b {
-        width: 100px; /* Fixed width for the label */
-        text-align: left; /* Align text to the right */
-    }
+  .id-item b {
+    width: 100px; /* Fixed width for the label */
+    text-align: left; /* Align text to the right */
+    padding-left: 10px;
+  }
 
-    .id-item span {
-        flex-grow: 1; /* Value takes the remaining space */
-        padding-left: 10px; /* Add padding to the left of the value */
-    }
+  .id-item span {
+    flex-grow: 1; /* Value takes the remaining space */
+    padding-left: 10px; /* Add padding to the left of the value */
+  }
 
-    #voterID {
-        grid-row: 1;
-    }
+  #voterID {
+    grid-row: 1;
+  }
 
-    #ballotID {
-        grid-row: 2;
-    }
+  #ballotID {
+    grid-row: 2;
+  }
 </style>
